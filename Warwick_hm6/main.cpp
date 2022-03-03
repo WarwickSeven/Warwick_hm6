29051383e676a19152e3fc493863a6bdea9423fd
WarwickSeven <WarwickSeven@gmail.com>
2022-03-03 14:42:22 +0300

    Домашнее задание 6-1

--- Warwick_hm6/main.cpp
+++ Warwick_hm6/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  Warwick_hm6
-//
-//  Created by Александр Тихонов on 02.03.2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using std::cout;
+using std::cin;
+using std::endl;
+
+void initArray(int *arr, int size)
+{
+    arr[0] = 1;
+    for (size_t i = 1; i < size; i++)
+        arr[i] = arr[i-1] << 1;
+}
+
+void PrintArray(int *arr, int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr1Size;
+    cout << "размер массива: ";
+    cin >> arr1Size;
+    int *array1 = new int[arr1Size];
+    
+    initArray(array1, arr1Size);
+    PrintArray(array1, arr1Size);
+    
+    delete[] array1;
+    array1 = nullptr;
+    
     return 0;
 }
